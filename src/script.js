a9c1a464a3a748765757b03f1a6cdab96c9c4caf
Ne_Uchi <drmor107@gmail.com>
2026-02-27 16:08:42 +0500

    now you can delete your projects froms array with DOM element

--- src/script.js
+++ src/script.js
@@ -5,10 +5,11 @@ const confirmBtn = document.getElementById("confirmBtn");
 const closeBtn = document.getElementById("closeBtn");
 const allProjects = document.querySelector(".projectSelect");
 
-const todos = [];
+let todos = [];
 
-function ToDo(name){
+function ToDo(name, uuid){
     this.name = name;
+    this.uuid = uuid;
 };
 
 addProject.addEventListener('click', () => {
@@ -18,23 +19,35 @@ addProject.addEventListener('click', () => {
 closeBtn.addEventListener("click", () => {
     popupWindow.style.display = "none";
 });
-function displayProjectsBtn(){
-    const newProject = document.createElement("button");
+function displayProjectsBtn(todo){
+    const newProject = document.createElement("div");
+    const projectBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+    newProject.classList.add("projectCard");
+    newProject.appendChild(projectBtn);
+    newProject.appendChild(deleteBtn);
     allProjects.appendChild(newProject);
-    newProject.textContent = todos.at(-1).name;
+    projectBtn.textContent = todo.name;
+    deleteBtn.innerHTML = `<span style="color: #F45B69;">X</span>`;
+
+    deleteBtn.addEventListener("click", () => {
+        todos = todos.filter(p => p.uuid !== todo.uuid);
+        newProject.remove();
+    });
 };
 function getName(){
     const projectName = document.getElementById("name").value;
-    const todo = new ToDo(projectName);
+    let uuid = self.crypto.randomUUID();
+    const todo = new ToDo(projectName, uuid);
     if (projectName == ''){
-        todos.push(new ToDo("unnamed"));
+        todos.push(new ToDo("unnamed", uuid));
     } else {
         todos.push(todo);
     }
+    displayProjectsBtn(todo)
 }
 confirmBtn.addEventListener("click", () => {
     getName();
     console.log(todos);
     popupWindow.style.display = "none";
-    displayProjectsBtn();
 });
